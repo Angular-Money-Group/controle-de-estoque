2773bdc001b48ca004381132ce188697c27cb979
GhabryelHenrique <ghabryelhenrique@hotmail.com>
2023-01-13 14:09:39 -0300

    Fix Cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
 
 const app = express();
 
@@ -14,13 +15,9 @@ app.use(express.json());
 
 app.use(productsRouter);
 app.use(authRouter);
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+app.use(cors({
+    origin: 'https://gestao-de-projetos.onrender.com'
+}));
 
 // Connect to MongoDB
 mongoose
@@ -35,4 +32,4 @@ app.get("/", (req, res) => {
   return res.send("Hello World");
 });
 
-app.listen(3000);
+app.listen();
