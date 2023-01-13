fb601fb28b6708ca08028f44e9ad8b63e1c19e33
GhabryelHenrique <ghabryelhenrique@hotmail.com>
2023-01-13 14:41:33 -0300

    Fix Cors

--- app.js
+++ app.js
@@ -15,9 +15,16 @@ app.use(express.json());
 
 app.use(productsRouter);
 app.use(authRouter);
-app.use(cors({
-    origin: 'https://gestao-de-projetos.onrender.com'
-}));
+app.use((req, res, next) => {
+  req.header("Access-Control-Allow-Origin", "*");
+
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 // Connect to MongoDB
 mongoose
