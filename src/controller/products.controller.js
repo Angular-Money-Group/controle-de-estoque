2419ae9a65f9444baa77c215fa1362410780d269
GhabryelHenrique <ghabryelhenrique@hotmail.com>
2023-02-07 08:27:44 -0300

    Fix barCode error

--- src/controller/products.controller.js
+++ src/controller/products.controller.js
@@ -14,21 +14,21 @@ module.exports = class ProductsController {
         let allProducts = [
           ...new Set([...productsByName, ...productsByBarCode]),
         ];
-        return res
-          .status(200)
-          .json({
-            message: "Operação realizada com sucesso",
-            data: allProducts,
-            totalItens: allProducts.length,
-          });
+        return res.status(200).json({
+          message: "Operação realizada com sucesso",
+          data: allProducts,
+          totalItens: allProducts.length,
+        });
       } catch (err) {
         return res.status(400).json({ message: "Erro ao consultar", err });
       }
     } else {
       let product = await productsSchema.find();
-      return res
-        .status(200)
-        .json({ message: "Operação realizada com sucesso", data: product, totalItens: product.length });
+      return res.status(200).json({
+        message: "Operação realizada com sucesso",
+        data: product,
+        totalItens: product.length,
+      });
     }
   }
 
@@ -49,141 +49,146 @@ module.exports = class ProductsController {
   }
 
   static async createProduct(req, res) {
-    let {
-      name,
-      priceCost,
-      priceSell,
-      barCode,
-      description,
-      category,
-      imageBase64,
-      initialStock,
-      moveStock,
-      realStock,
-      createdAt,
-      updatedAt,
-    } = req.body;
-
-    //validations
-    if (createdAt) {
-      return res
-        .status(422)
-        .json({
-          message: "Não é permitido enviar a data de criação, ela será automatica!",
-        });
-    }
-
-    let productByBarCode = productsSchema.find({
-      barCode: { $regex: req.query.filter, $options: "i" },
-    });
-
-    if(productByBarCode){
-      return res.status(422).json({ message: "Código de barras ja cadastrado" });
-    }
-
-    if (updatedAt) {
-      return res
-        .status(422)
-        .json({
-          message: "Não é permitido enviar a data de atualização, ela será automatica!",
-        });
-    }
-
-    if (!name) {
-      return res.status(422).json({ message: "Nome é obrigatorio" });
-    }
-
-    if (!priceCost) {
-      return res.status(422).json({ message: "Preço de custo é obrigatorio" });
-    }
-
-    if (!priceSell) {
-      return res.status(422).json({ message: "Preço de venda é obrigatorio" });
-    }
-
-    if (!barCode) {
-      return res.status(422).json({ message: "Codigo de Barras é obrigatorio" });
-    }
-
-    if (!category) {
-      return res.status(422).json({ message: "Categoria é obrigatorio" });
-    }
-
-    if (!initialStock) {
-      return res.status(422).json({ message: "Estoque Inicial é obrigatorio" });
-    }
-
-    // Add createdAt and stock
-    realStock = initialStock;
-    createdAt = new Date(Date.now());
-
-    const product = new productsSchema({
-      name,
-      priceCost,
-      priceSell,
-      barCode,
-      description,
-      category,
-      imageBase64,
-      initialStock,
-      moveStock,
-      realStock,
-      createdAt,
-      updatedAt,
-    });
-
     try {
+      let {
+        name,
+        priceCost,
+        priceSell,
+        barCode,
+        description,
+        category,
+        imageBase64,
+        initialStock,
+        moveStock,
+        realStock,
+        createdAt,
+        updatedAt,
+      } = req.body;
+
+
+
+      let productsByBarCode = await productsSchema.find({
+        barCode: { $regex: barCode, $options: "i" },
+      });
+
+      if (productsByBarCode.length > 0) {
+        return res.status(422).json({
+          message: "Já existe um produto com esse codigo de barras",
+        });
+      }
+
+      if (updatedAt) {
+        return res.status(422).json({
+          message:
+            "Não é permitido enviar a data de atualização, ela será automatica!",
+        });
+      }
+
+      if (!name) {
+        return res.status(422).json({ message: "Nome é obrigatorio" });
+      }
+
+      if (!priceCost) {
+        return res
+          .status(422)
+          .json({ message: "Preço de custo é obrigatorio" });
+      }
+
+      if (!priceSell) {
+        return res
+          .status(422)
+          .json({ message: "Preço de venda é obrigatorio" });
+      }
+
+      if (!barCode) {
+        return res
+          .status(422)
+          .json({ message: "Codigo de Barras é obrigatorio" });
+      }
+
+      if (!category) {
+        return res.status(422).json({ message: "Categoria é obrigatorio" });
+      }
+
+      if (!initialStock) {
+        return res
+          .status(422)
+          .json({ message: "Estoque Inicial é obrigatorio" });
+      }
+
+      // Add createdAt and stock
+      realStock = initialStock;
+      createdAt = new Date(Date.now());
+
+      const product = new productsSchema({
+        name,
+        priceCost,
+        priceSell,
+        barCode,
+        description,
+        category,
+        imageBase64,
+        initialStock,
+        moveStock,
+        realStock,
+        createdAt,
+        updatedAt,
+      });
+
       await product.save();
       return res
         .status(200)
         .json({ message: "Produto cadastrado com sucesso!", data: product });
     } catch (err) {
       console.log(err);
-      return res
-        .status(500)
-        .json({ message: "Erro Interno do servidor, tente novamente mais tarde" });
+      return res.status(500).json({
+        message: "Erro Interno do servidor, tente novamente mais tarde",
+      });
     }
   }
 
   static async updateProduct(req, res) {
-    try{
-    const { name, priceCost, priceSell, description, category, moveStock } =
-      req.body;
-    const { id } = req.params;
+    try {
+      const { name, priceCost, priceSell, description, category, moveStock } =
+        req.body;
+      const { id } = req.params;
 
-    if (!id) {
-      return res.status(422).json({ message: "ID não informado" });
-    }
+      if (!id) {
+        return res.status(422).json({ message: "ID não informado" });
+      }
 
-    if(!moveStock){
-      return res.status(422).json({ message: "Movimento de estoque não informado" });
-    }
+      if (!moveStock) {
+        return res
+          .status(422)
+          .json({ message: "Movimento de estoque não informado" });
+      }
 
-    const product = await productsSchema.findById(id);
+      const product = await productsSchema.findById(id);
 
-    if (!product) {
-      return res.status(404).json({ message: "Produto não encontrado" });
-    }
+      if (!product) {
+        return res.status(404).json({ message: "Produto não encontrado" });
+      }
 
-    product.name = name;
-    product.priceCost = priceCost;
-    product.priceSell = priceSell;
-    product.description = description;
-    product.category = category;
-    product.moveStock = moveStock;
-    product.realStock += moveStock;
-    product.updatedAt = Date.now()
+      product.name = name;
+      product.priceCost = priceCost;
+      product.priceSell = priceSell;
+      product.description = description;
+      product.category = category;
+      product.moveStock = moveStock;
+      product.realStock += moveStock;
+      product.updatedAt = Date.now();
 
-    await product.save();
+      await product.save();
 
-    return res
-      .status(200)
-      .json({ message: "Produto editado com sucesso", data: product });
+      return res
+        .status(200)
+        .json({ message: "Produto editado com sucesso", data: product });
     } catch (err) {
       console.log(err);
-      return res
-        .status(500)
-        .json({ message: "Erro Interno do servidor, tente novamente mais tarde", err });
+      return res.status(500).json({
+        message: "Erro Interno do servidor, tente novamente mais tarde",
+        err,
+      });
     }
   }
 
