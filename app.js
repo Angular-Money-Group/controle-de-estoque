530df656f3d1d5a365586c6ab34bd31921486a65
Ghabryel <ghabryelhenrique@hotmail.com>
2023-02-22 13:57:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const express = require("express");
 const mongoose = require("mongoose");
