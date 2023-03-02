b6fedad390a32fe239984acdcb5a89e811b956ab
GhabryelHenrique <ghabryelhenrique@hotmail.com>
2023-03-02 14:11:42 -0300

    Add all days in home page

--- src/controller/home.controller.js
+++ src/controller/home.controller.js
@@ -8,7 +8,40 @@ module.exports = class HomeController {
       const startDate = new Date(req.query.startDate);
       const endDate = new Date(req.query.endDate);
 
-      var totalSell = { allDays: 0, days: [] };
+      var totalSell = { allDays: 0, 
+        days: [
+          {day: 1, value: 0},
+          {day: 2, value: 0},
+          {day: 3, value: 0},
+          {day: 4, value: 0},
+          {day: 5, value: 0},
+          {day: 6, value: 0},
+          {day: 7, value: 0},
+          {day: 8, value: 0},
+          {day: 9, value: 0},
+          {day: 10, value: 0},
+          {day: 11, value: 0},
+          {day: 12, value: 0},
+          {day: 13, value: 0},
+          {day: 14, value: 0},
+          {day: 15, value: 0},
+          {day: 16, value: 0},
+          {day: 17, value: 0},
+          {day: 18, value: 0},
+          {day: 19, value: 0},
+          {day: 20, value: 0},
+          {day: 21, value: 0},
+          {day: 22, value: 0},
+          {day: 23, value: 0},
+          {day: 24, value: 0},
+          {day: 25, value: 0},
+          {day: 26, value: 0},
+          {day: 27, value: 0},
+          {day: 28, value: 0},
+          {day: 29, value: 0},
+          {day: 30, value: 0},
+          {day: 31, value: 0},
+        ] };
       var totalStock = 0;
       var totalPatrimony = 0;
 
@@ -40,7 +73,11 @@ module.exports = class HomeController {
         if(element.state === 'Fechado') {
           element.createdAt = new Date(element.createdAt);
           totalSell.allDays += element.totalSell;
-          totalSell.days.push({day: element.createdAt.getDate(), value: element.totalSell});
+          totalSell.days.forEach((day) => {
+            if(day.day === element.createdAt.getDate()) {
+              day.value = element.totalSell;
+            }
+          });
         };
       });
 
